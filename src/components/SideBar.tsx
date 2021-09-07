873a38e6372553605a80891669026f1f1830a810
Taise Soares <contato@taisesoares.com>
2021-09-07 16:21:40 -0300

    refactor: sidebar and content create component
    Signed-off-by: Taise Soares <contato@taisesoares.com>

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  action: (id: number) => void
+  selectedGenreId: number
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.action(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
